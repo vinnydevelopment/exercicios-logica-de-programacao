b5ba918a981b3f31753dc5eb1649fdd366fff544
Paulo <pauloviniciusoliveiradacosta96@gmail.com>
2022-08-21 18:23:15 -0300

    21-08-2022

--- 2-estrutura-de-decisão/18.py
+++ 2-estrutura-de-decisão/18.py
@@ -1,7 +1,1 @@
 # Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida.
-
-dia, mes, ano = input().replace('/','')
-
-print(dia)
-print(mes)
-print(ano)
--- 2-estrutura-de-decisão/22.py
+++ 2-estrutura-de-decisão/22.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+
+numero = int(input("Informe um número: "))
+
+if numero % 2 == 0:
+    tipo = "par"
+else:
+    tipo = "impar"
+print(f"O número {numero} é {tipo}!")
